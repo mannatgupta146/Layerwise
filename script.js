ed7dd37b539a3d9e209a542f73dd6fa601a6e342
mannatgupta146 <mannatgupta146@gmail.com>
2025-06-24 23:52:37 +0530

    added

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-Shery.mouseFollower({
-  skew: true,
-  skewFactor: 0.25,
-  ease: "cubic-bezier(0.25, 1, 0.5, 1)",
-  duration: 0.6,
-  scale: 1.2,
-  delay: 0.1,
-  hideNativeCursor: true
-});
+Shery.mouseFollower();
