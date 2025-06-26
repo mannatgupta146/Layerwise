768705e2ccfce3fd3d52b1d36878af60f2c51ba1
mannatgupta146 <mannatgupta146@gmail.com>
2025-06-26 11:37:56 +0530

    added

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 Shery.mouseFollower();
 
+Shery.makeMagnet("nav h5" );
+
+Shery.textAnimate("nav h4", {
+  style: 1,
+  y: 5,
+  delay: 0.1,
+  duration: 1,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 1,
+});
+
+Shery.imageMasker("nav button", {
+  mouseFollower: true,
+  text: "Click Me",
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
 function navAnimation() {
     const nav = document.querySelector("nav");
     const navBottom = document.querySelector("#nav-bottom");
