55c316a51b5699f5751ca43c2fa8bd8eb7f813c3
mannatgupta146 <mannatgupta146@gmail.com>
2025-07-01 01:22:50 +0530

    added

--- script.js
+++ script.js
@@ -77,27 +77,30 @@ function navAnimation() {
 navAnimation()
 
 
-var rightElems = document.querySelectorAll(".right-elem")
+function page2Annimation() {
+    var rightElems = document.querySelectorAll(".right-elem");
 
-rightElems.forEach((elem) => {
-    elem.addEventListener("mouseenter", function() {
-        gsap.to(elem.childNodes[3],{
-            opacity: 1,
-            scale: 1
+    rightElems.forEach((elem) => {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            });
+        });
+
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            });
+        });
+
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[3], {
+                x: dets.clientX - elem.getBoundingClientRect().x - 50,
+                y: dets.clientY - elem.getBoundingClientRect().y - 150
+            });
         });
     });
-
-    elem.addEventListener("mouseleave", function() {
-        gsap.to(elem.childNodes[3],{
-            opacity: 0,
-            scale: 0
-        });
-    });
-
-    elem.addEventListener("mousemove", function(dets) {
-        gsap.to(elem.childNodes[3], {
-            x: dets.x - elem.getBoundingClientRect().x - 50,
-            y: dets.y - elem.getBoundingClientRect().y -150
-        });
-    })
-})
+}
+page2Annimation();
