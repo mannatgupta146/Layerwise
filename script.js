504bd75d2a6114a72a1f1ef8dbdc5f079076cf13
mannatgupta146 <mannatgupta146@gmail.com>
2025-07-13 02:02:26 +0530

    added

--- script.js
+++ script.js
@@ -204,5 +204,28 @@ function page6Animations() {
 page6Animations();  
 
 function loadingAnimation() {
-    
+
+    var tl = gsap.timeline()
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.2
+    })
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "100px",
+        duration: 2,
+        ease: "expo.out"
+    })
+    tl.from("nav", {
+        opacity: 0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1, #page1 p, #page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
 }
+loadingAnimation();
+
